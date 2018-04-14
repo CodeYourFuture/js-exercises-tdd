9dbcdd1b30173c9176416b420d8d4296da50bf28
Islam Dudaev <islamwolf23@gmail.com>
2018-04-14 12:51:48 +0100

    First test.

--- I.write-tests/01-greet-people/greet-people.test.js
+++ I.write-tests/01-greet-people/greet-people.test.js
@@ -1,7 +1,10 @@
+var greetPeople = require('./greet-people');
+
 test("print list of names prefixed with Hello", function() {
     // Arrange
-    
+    var mentors = ['irina', 'ashleigh', 'etza'];
     // Act
-    
-	// Assert
+    var result = greetPeople(mentors);
+    // Assert
+    expect(result).toEqual('Hello irinaashleighetza');
 });
