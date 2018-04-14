e6825b192ae99976de5180589fdb87324bbe2b9a
Islam Dudaev <islamwolf23@gmail.com>
2018-04-14 14:47:29 +0100

    ex 5 done

--- I.write-tests/02-remove-vowels/remove-vowels.test.js
+++ I.write-tests/02-remove-vowels/remove-vowels.test.js
@@ -1,8 +1,11 @@
+var greetPeople = require("./remove-vowels");
+
 test('remove vowels from word', function () {
 
     // Arrange
-
+    var samuel = "samuel";
     // Act
-
+    var result = removeVowels(samuel);
     // Assert
+    expect(result).toEqual("_a_ue_");
 })
--- I.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ I.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -1,5 +1,26 @@
 // example
 // input = [90, 5, 11, 8, 6];
+
+var x = require("./get-second-third");
+
 // expected output = [6, 8];
 
+test("returns second and third largest", function(){
+    var input = [90, 5, 11, 8, 6];
+    var result = sut(input);
+    expect(result).toEqual([6,8]);
+});
+
 // also test that the original array has not changed
+
+test("array does not change", function() {
+    var input = [90, 5, 11, 8, 6];
+    var originalInput = [90, 5, 11, 8, 6];
+    
+	var result = sut(input);
+	expect(input).toEqual(originalInput);
+});
+
+
+
+
