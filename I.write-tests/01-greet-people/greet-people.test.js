da49f6c2a7a4b2b562b044a8d342902df43609f1
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-04-14 14:22:33 +0100

    greet-people

--- I.write-tests/01-greet-people/greet-people.test.js
+++ I.write-tests/01-greet-people/greet-people.test.js
@@ -1,7 +1,9 @@
-test('print list of names prefixed with Hello', function () {
-    // Arrange
-
-    // Act
-
-    // Assert
-})
+var greetPeople = require("./greet-people");
+test("print list of names prefixed with Hello", function() {
+  // Arrange
+  var array = ["ashelia", "daniel", "hiwot"];
+  // Act
+  var result = greetPeople(array);
+  // Assert
+  expect(result).toEqual("Hello asheliadanielhiwot");
+});
