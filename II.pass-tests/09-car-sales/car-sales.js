6b0521556eb76315c99867e39eb8f53d2516b383
Islam Dudaev <islamwolf23@gmail.com>
2018-04-15 19:07:20 +0100

    Complete 10 folder in pass-tests

--- II.pass-tests/09-car-sales/car-sales.js
+++ II.pass-tests/09-car-sales/car-sales.js
@@ -25,14 +25,4 @@ function sales(cars) {
     return totals;
 }
 
-var carsSold = [
-	{ make: "Ford", model: "Fiesta", colour: "Red", price: 5999 },
-	{ make: "Land Rover", model: "Defender", colour: "Muddy", price: 12000 },
-	{ make: "Toyota", model: "Prius", colour: "Silver", price: 6500 },
-	{ make: "Honda", model: "Civic", colour: "Yellow", price: 8000 },
-	{ make: "Ford", model: "Fiesta", colour: "Red", price: 15000 },
-	{ make: "Land Rover", model: "Discovery", colour: "Blue", price: 9000 },
-	{ make: "Ford", model: "Fiesta", colour: "Green", price: 2000 }
-];
-console.log(sales(carsSold));
 module.exports = sales;
