54a6a45cd6bc877886d8365628aa6eb3668c3680
Islam Dudaev <islamwolf23@gmail.com>
2018-04-15 17:47:30 +0100

    Complete 08 folder in pass-tests

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,7 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+function getAverage(numbers) {
+	return numbers.filter(num => Number(num)).length;
+}
+module.exports = getAverage;
