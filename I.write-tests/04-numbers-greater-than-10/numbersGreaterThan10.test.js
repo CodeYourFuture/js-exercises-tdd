e195c8a7e79bee79b1a0ad828c2df5e8dbde1137
Islam Dudaev <islamwolf23@gmail.com>
2018-04-15 15:59:33 +0100

    Complete 04 folder in write-tests

--- I.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ I.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -2,6 +2,12 @@ var largerThanTen = require('./numbersGreaterThan10');
 
 test('Get numbers greater than 10', function () {
 
+    var input = [4, 10, 32, 9, 21];
+    var output = [32, 21];
+
+    var result = largerThanTen(input);
+
+    expect(result).toEqual(output);
 });
 
 
