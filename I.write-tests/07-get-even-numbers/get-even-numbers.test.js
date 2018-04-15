aa763d517734df974a1ec2153cbd6d9f77305a49
Islam Dudaev <islamwolf23@gmail.com>
2018-04-15 16:25:46 +0100

    Complete 07 folder in write-tests

--- I.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ I.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,11 @@
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
+
+var getEven = require("./get-even-numbers");
+
+test("get even numbers", function(){
+    var input = [22, 13, 73, 82, 4];
+    var result = getEven(input);
+    expect(result).toEqual([22, 82, 4]);
+});
