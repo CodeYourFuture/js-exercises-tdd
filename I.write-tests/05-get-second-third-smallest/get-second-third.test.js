94d9860db8d52ef85dca70a73bcdce0168caa6a4
Islam Dudaev <islamwolf23@gmail.com>
2018-04-15 16:13:05 +0100

    Complete 05 folder in write-tests

--- I.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ I.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -1,13 +1,13 @@
 // example
 // input = [90, 5, 11, 8, 6];
 
-var x = require("./get-second-third");
+var getSecondAndThird = require("./get-second-third");
 
 // expected output = [6, 8];
 
 test("returns second and third largest", function(){
     var input = [90, 5, 11, 8, 6];
-    var result = sut(input);
+    var result = getSecondAndThird(input);
     expect(result).toEqual([6,8]);
 });
 
@@ -17,7 +17,7 @@ test("array does not change", function() {
     var input = [90, 5, 11, 8, 6];
     var originalInput = [90, 5, 11, 8, 6];
     
-	var result = sut(input);
+	var result = getSecondAndThird(input);
 	expect(input).toEqual(originalInput);
 });
 
