1d16e0c385fef3814fa79b0a0987e34ba909dc3d
Islam Dudaev <islamwolf23@gmail.com>
2018-04-17 01:56:10 +0100

    Complete second test

--- III.tdd-katas/password-verifier/passwordVerifier.js
+++ III.tdd-katas/password-verifier/passwordVerifier.js
@@ -1,12 +1,21 @@
 class PasswordVerifier {
     Verify(password){
-        return password.length > 8;
+        if (password === null || password === "") {
+			console.log("Password should not be null or empty");
+			return false;
+		} else if(password.length < 8){
+            console.log("Password must be larger than 8 chars");
+            return false;
+        } else {
+            return true;
+        }
+
     }
 }
 
 let verifier = new PasswordVerifier();
 module.exports = verifier.Verify;
 
-let bool = verifier.Verify("Islamsfddf");
+let bool = verifier.Verify("dsf");
 console.log(bool);
 
--- III.tdd-katas/password-verifier/passwordVerifier.test.js
+++ III.tdd-katas/password-verifier/passwordVerifier.test.js
@@ -1,10 +1,14 @@
 var verify = require("./passwordVerifier");
 
 test("1. - password should be larger than 8 chars", function() {
-    var password = "islam234324"
+    var password = "islam";
 	var result = verify(password);
-	expect(result).toEqual(true);
+	expect(result).toEqual(false);
 });
 
-
+test("2. - password should not be null", function() {
+	var password = null;
+	var result = verify(password);
+	expect(result).toEqual(false);
+});
 
