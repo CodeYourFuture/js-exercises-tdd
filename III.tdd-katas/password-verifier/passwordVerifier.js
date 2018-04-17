74b8320c555988148c6bc9abccd1a4864499e265
Islam Dudaev <islamwolf23@gmail.com>
2018-04-17 14:23:08 +0100

    Complete first part

--- III.tdd-katas/password-verifier/passwordVerifier.js
+++ III.tdd-katas/password-verifier/passwordVerifier.js
@@ -1,21 +1,31 @@
+// Create a Password verifications class called “PasswordVerifier”.
 class PasswordVerifier {
+    // 1. Add the following verifications to a master function called “Verify()”
     Verify(password){
+        var result;
         if (password === null || password === "") {
 			console.log("Password should not be null or empty");
-			return false;
+            result = false;
 		} else if(password.length < 8){
             console.log("Password must be larger than 8 chars");
-            return false;
+            result = false;
+        } else if(!password.match(/[A-Z]+/)){
+            console.log("Password should have one uppercase letter at least");
+            result = false;
+        } else if(!password.match(/[a-z]+/)){
+            console.log("Password should have one lowercase letter at least");
+            result = false;
+        } else if(!password.match(/\d+/)){
+            console.log("Password should have one number at least");
+            result = false;
         } else {
-            return true;
+            result = true;
         }
-
+        return result;
     }
 }
 
 let verifier = new PasswordVerifier();
 module.exports = verifier.Verify;
 
-let bool = verifier.Verify("dsf");
-console.log(bool);
 
--- III.tdd-katas/password-verifier/passwordVerifier.test.js
+++ III.tdd-katas/password-verifier/passwordVerifier.test.js
@@ -1,14 +1,36 @@
 var verify = require("./passwordVerifier");
 
 test("1. - password should be larger than 8 chars", function() {
-    var password = "islam";
-	var result = verify(password);
-	expect(result).toEqual(false);
+	var result = verify("iSl5am");
+	var result2 = verify("iSlasdfsm5");
+    expect(result).toEqual(false);
+    expect(result2).toEqual(true);
 });
 
 test("2. - password should not be null", function() {
-	var password = null;
-	var result = verify(password);
+	var result = verify(null);
+	var result2 = verify("");
 	expect(result).toEqual(false);
+	expect(result2).toEqual(false);
 });
 
+test("3. - password should have one uppercase letter at least", function() {
+	var result = verify("L5sfsfsfsfaf");
+	var result2 = verify("5sfsfsfsfaf");
+	expect(result).toEqual(true);
+	expect(result2).toEqual(false);
+});
+
+test("4. - password should have one lowercase letter at least", function() {
+	var result = verify("L5sfsfsfsfaf");
+	var result2 = verify("5234324SFDSF");
+	expect(result).toEqual(true);
+	expect(result2).toEqual(false);
+});
+
+test("5. - password should have one number at least", function() {
+	var result = verify("fdfdfgDd4fd");
+	var result2 = verify("fdfdfgDdfd");
+	expect(result).toEqual(true);
+	expect(result2).toEqual(false);
+});
