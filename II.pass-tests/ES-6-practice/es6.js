bb8215537e9cee6219eb8b94f57276ee18f08141
Islam Dudaev <islamwolf23@gmail.com>
2018-04-21 21:18:25 +0100

    es6 exercise start

--- II.pass-tests/ES-6-practice/es6.js
+++ II.pass-tests/ES-6-practice/es6.js
@@ -1,28 +1,17 @@
 // Turn this function into an arrow function
-function arrow() {
-  return "es6 is awesome!";
-}
+const arrow = () => `es6 is awesome!`;
 
 //Use the es6 syntax for default parameter
-function defaultParameter(name) {
-  var name = name || "sam";
-  return name;
-}
+const defaultParameter = (name = "sam") => name;
 
 // Use the spread operator to combine arr1 and arr2
-function combineArrays(arr1, arr2) {
-  return arr1.concat(arr2);
-}
+const combineArrays = (arr1, arr2) => arr1.concat(arr2);
 
 //use destructuring to return the object's cyf property
-function destructuring(obj) {
-  return obj.cyf;
-}
+const destructuring = (obj) => obj.cyf;
 
 // use template literal to return a string with the sum of a and b
-function templateString(a, b) {
-  return "The sum is equal to " + (a + b).toString();
-}
+const templateString = (a, b) => `The sum is equal to ${(a + b).toString()}`;
 
 module.exports = {
   arrow,
