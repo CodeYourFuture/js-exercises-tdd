3fc2556559165dcb135e44f1e8e5f735cfb143a5
Seraphine Young <seraphine_ene@yahoo.com>
2018-07-05 21:42:31 +0100

    test part 2 in progress

--- II.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ II.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,5 +1,5 @@
-var getWordLengths = function (someWords) {
-
+var getWordLengths = function(someWords) {
+  return someWords.map(item => item.length);
 };
 
 module.exports = getWordLengths;
--- II.pass-tests/02-get-word-lengths/get-word-lengths.test.js
+++ II.pass-tests/02-get-word-lengths/get-word-lengths.test.js
@@ -1,9 +1,9 @@
-var wordLengths = require('./get-word-lengths');
+var wordLengths = require("./get-word-lengths");
 
-test('Get word lengths', function () {
-    var words = ['sun', 'potato', 'roundabout', 'pizza'];
-    var expected = [3, 6, 10, 5];
+test("Get word lengths", function() {
+  var words = ["sun", "potato", "roundabout", "pizza"];
+  var expected = [3, 6, 10, 5];
 
-    var output = wordLengths(words);
-    expect(output).toEqual(expected);
+  var output = wordLengths(words);
+  expect(output).toEqual(expected);
 });
