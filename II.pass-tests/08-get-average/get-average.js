e96f079254f413e3aeadcd72fefad8eb8eb7e088
clement <clemohiomoje@gmail.com>
2019-02-20 23:59:13 +0000

    get average 'number' exercise /test.js

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,22 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+function average(numbers){
+    var sum = 0;
+    const arrayNumbers = numbers.filter(number => {
+        return typeof number === "number";
+    } )
+    arrayNumbers.forEach(number => {
+    return sum += number;
+    });
+return sum / arrayNumbers.length;
+}
+module.exports = average;
+
+
+//for(var i =0; i < numbers.length; i++){
+    //     sum += Number(numbers[i], 10)
+    //     return sum;
+    // }
+    // var avg = numbers.length / sum;
+    //  return avg
--- II.pass-tests/08-get-average/get-average.test.js
+++ II.pass-tests/08-get-average/get-average.test.js
@@ -1,7 +1,7 @@
 var average = require('./get-average');
 
 test('Average', function () {
-    var numbers = [4, '-', 8, 11, 'hello', '57', 0, 2];
+    var numbers = [4, '-', 8, 11, 'hello', '57', 0, 2] ;
     var expected = 5;
 
     var output = average(numbers);
