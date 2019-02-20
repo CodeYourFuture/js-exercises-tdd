7b679abeea37e3ddf005988e20f8d719ad1c90cf
clement <clemohiomoje@gmail.com>
2019-02-20 23:48:42 +0000

    add one exercise / test

--- II.pass-tests/01-add-one/add-one.js
+++ II.pass-tests/01-add-one/add-one.js
@@ -1,3 +1,9 @@
 module.exports = function (numbers) {
+        var result = [];
 
+    for (var i = 0; i < numbers.length; i++) {
+        var value = numbers[i];
+            result.push(value + 1);
+    }
+    return result;
 };
--- II.pass-tests/01-add-one/add-one.test.js
+++ II.pass-tests/01-add-one/add-one.test.js
@@ -1,6 +1,6 @@
 var addOne = require('./add-one.js');
 
-test('Add 1 to each item in myArray', function () {
+test('Add 1 to each item in myArray',  function () {
     var myArray = [31, 57, 12, 5];
 
     var unchanged = [31, 57, 12, 5];
