04c188d0ba9ae0053e320ab3cb21d1d549053c94
Kadir <kegilmez@gmail.com>
2019-02-21 23:23:23 +0000

    fizzbuzz-another_solution

--- III.tdd-katas/fizz-buzz/fizz-buzz.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -19,14 +19,15 @@ module.exports = function (number) {
 */
 
 // step-2
+/*
 function fizzbuzz(number) {
     var result = [];
     for (var i = 1; i <= number; i++) {
-        if (isInculed(i, 3) && isInculed(i, 5) || isDividedBy(i, 3) && isDividedBy(i, 5)) {
+        if (isInculed(3, i) && isInculed(5, i) || isDividedBy(i, 3) && isDividedBy(i, 5)) {
             result[i] = "FizzBuzz";
-        } else if (isInculed(i, 3) || isDividedBy(i, 3)) {
+        } else if (isInculed(3, i) || isDividedBy(3, i)) {
             result[i] = "Fizz";
-        } else if (isInculed(i, 5) || isDividedBy(i, 5)) {
+        } else if (isInculed(3, i) || isDividedBy(5, i)) {
             result[i] = "Buzz";
         } else {
             result[i] = i;
@@ -35,11 +36,31 @@ function fizzbuzz(number) {
     result.shift();
     return result.join(", ");
 }
-// function to check if the 3 or 5 icluded inside the number
-function isInculed(number, threeOrfive) {
-    return number.toString().includes(threeOrfive);
+*/
+
+//step-2 another solution
+function fizzbuzz(number) {
+    var result = [];
+    for (var i = 1; i <= number; i++) {
+        if (isInculed(3, i) && isInculed(5, i) || isDividedBy(i, 3) && isDividedBy(i, 5)) {
+            result.push("FizzBuzz");
+        } else if (isInculed(3, i) || isDividedBy(i, 3)) {
+            result.push("Fizz");
+        } else if (isInculed(3, i) || isDividedBy(i, 5)) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        } //if
+    } //for
+    return result.join(", ");
 }
 
+
+// function to check if any number is icluded inside the number
+function isInculed(threeOrfive, number) {
+    return number.toString().includes(threeOrfive);
+}
+// function to check if the number is divisible by any number
 function isDividedBy(number, divisor) {
     return number % divisor === 0;
 }
