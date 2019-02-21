12c59d087053fa64c271fea0cc9d010f3200f79f
ZaimAbz <zaimabodi@gmail.com>
2019-02-21 19:27:55 +0000

    Abz 07

--- I.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ I.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,10 @@
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
+var getEvenNumbers = require("./get-even-numbers");
+test("Get even numbers", function() {
+  const numbers = [22, 13, 73, 82, 4];
+  const expectation = [22, 82, 4];
+  const result = getEvenNumbers(numbers);
+  expect(result).toEqual(expectation);
+});
