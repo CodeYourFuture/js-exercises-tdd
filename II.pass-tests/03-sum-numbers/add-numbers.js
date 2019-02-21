29d716bcae3f953294a6839f82422706ecc9a485
ahmet <ahmet.kio@hotmail.com>
2019-02-21 20:12:34 +0000

    Ahmet-JS-Core 3

--- II.pass-tests/03-sum-numbers/add-numbers.js
+++ II.pass-tests/03-sum-numbers/add-numbers.js
@@ -1,4 +1,16 @@
 function addNumbers(numbers) {
-
+/*    var total=0;
+for (i=0; i<numbers.length; i++){
+    total = total + numbers[i];
+}
+return total;
+*/
+total=0;
+total= numbers.reduce(
+     function (total, number) { 
+        return total+number;   
+        }
+     );
+return total;
 }
 module.exports = addNumbers;
