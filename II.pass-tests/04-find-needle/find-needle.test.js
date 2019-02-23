cf2760ffbdf691b7f62bdd6c816e0b924743d120
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-02-23 22:33:00 +0000

    Change test to check for passed word instead of implicit "needle"

--- II.pass-tests/04-find-needle/find-needle.test.js
+++ II.pass-tests/04-find-needle/find-needle.test.js
@@ -4,6 +4,14 @@ test("Find the needle", function() {
   var words = ["house", "train", "slide", "needle", "book"];
   var expected = 3;
 
-  var output = findTheNeedle(words);
+  var output = findTheNeedle(words, "needle");
+  expect(output).toEqual(expected);
+});
+
+test("Find the plant", function() {
+  var words = ["plant", "shelf", "arrow", "bird"];
+  var expected = 0;
+
+  var output = findTheNeedle(words, "plant");
   expect(output).toEqual(expected);
 });
