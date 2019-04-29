dea3cb920b1690cd9e34576e1020a2ee4d300013
nabraha <nebaytedros@yahoo.com>
2019-04-29 20:14:44 +0100

    01-greet-people-test

--- II.write-tests/01-greet-people/greet-people.js
+++ II.write-tests/01-greet-people/greet-people.js
@@ -1,8 +1,13 @@
 function greetPeople(people) {
   var greeting = "Hello ";
 
-  people.forEach(function(person) {
-    greeting = greeting + person;
+  people.forEach(function(person, idx) {
+    var lastElement = idx === people.length - 1;
+    if (lastElement) {
+      greeting = greeting + "" + person;
+    } else {
+      greeting = greeting + "" + person + ", ";
+    }
   });
 
   return greeting;
