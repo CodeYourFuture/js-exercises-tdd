c8906235cc61a938a80563e1ece7713eff418b14
Flaminiat <47352051+Flaminiat@users.noreply.github.com>
2019-09-03 14:52:30 +0200

    Delete remove-vowels.test.js
    p

--- 02-remove-vowels/remove-vowels.test.js
+++
@@ -1,7 +0,0 @@
-var removeVowels = require("./remove-vowels");
-
-test("remove vowels from word", function() {
-  var name = "samuel";
-  var result = removeVowels(name);
-  expect(result).toEqual("sml");
-});
