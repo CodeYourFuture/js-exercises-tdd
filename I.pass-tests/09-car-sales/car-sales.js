5177fc74ca85078221b1755543db7b14e9184cbd
mesutBE <gelukkig.zijn.be@gmail.com>
2020-03-07 13:40:27 +0100

    ex-9-11 update

--- I.pass-tests/09-car-sales/car-sales.js
+++ I.pass-tests/09-car-sales/car-sales.js
@@ -1,5 +1,13 @@
-function sales(carsSold){
-    // couldn't understand
+function sales(carsSold) {
+    let output = {};
+    for (let obj of carsSold) {
+        if (output.hasOwnProperty(obj.make)) {
+            output[obj.make] = output[obj.make] + obj.price;
+        } else {
+            output[obj.make] = obj.price;
+        }
+    }
+    return output;
 }
 
 module.exports = sales;
