4372d5b579d8dcbad3b6b87097526629a8ad4444
thomashaile <gthomashaile@gmail.com>
2020-03-10 22:09:53 +0100

    test

--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -1,6 +1,17 @@
 var largerThanTen = require("./numbersGreaterThan10");
 
-test("Get numbers greater than 10", function() {});
+test("Get numbers greater than 10", function() {
+  // Arrange
+  var numbers = [4, 10, 32, 9, 21];
+  var expected = [32, 21];
+  // Act
+  var result = largerThanTen(numbers);
+  // Assert
+  expect(result).toEqual(expected);
+});
 
 // input: [4, 10, 32, 9, 21];
 // expected output: [32, 21];
+
+
+//it is correctly working since our test and code is pure test is passed
