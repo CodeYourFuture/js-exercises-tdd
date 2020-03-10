89dd59e4232df2f5f14d7f8cf532a103d42f5587
thomashaile <gthomashaile@gmail.com>
2020-03-10 22:11:22 +0100

    test

--- II.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ II.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -1,5 +1,21 @@
+var getSecondThird = require("./get-second-third");
+test("get second and third from array", function() {
+  // Arrange
+  var input = [90, 5, 11, 8, 6];
+  var unchanged = [90, 5, 11, 8, 6];
+  var expected = [6, 8];
+  // Act
+  var result = getSecondThird(input);
+  // Assert
+  expect(result).toEqual(expected);
+  expect(unchanged).toEqual(input);
+});
 // example
 // input = [90, 5, 11, 8, 6];
 // expected output = [6, 8];
 
-// also test that the original array has not changed
+/*
+our test passed and our code is pure no bugs
+
+
+*/
