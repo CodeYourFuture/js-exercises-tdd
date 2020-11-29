5f3378e2b3be6f1c897e07300340440ec664d117
marc.carvalho <marc.carvalho@inteligenciaweb.com.br>
2020-11-28 22:26:55 -0300

    1 exercise done

--- I.pass-tests/01-add-one/add-one.test.js
+++ I.pass-tests/01-add-one/add-one.test.js
@@ -1,12 +1,19 @@
 var addOne = require("./add-one.js");
 
 test("Add 1 to each item in myArray", function() {
-  var myArray = [31, 57, 12, 5];
+  var myArray = [-1, 57, 12, 5];
 
-  var unchanged = [31, 57, 12, 5];
-  var expected = [32, 58, 13, 6];
+
+  var unchanged = [-1, 57, 12, 5];
+  var expected = [0, 58, 13, 6];
   var output = addOne(myArray);
+
 
   expect(output).toEqual(expected);
   expect(myArray).toEqual(unchanged);
 });
+
+test('should throw error if there are no numbers in the array', () => {
+  const myArray = []
+  expect(() => addOne(myArray)).toThrow(Error)
+})
