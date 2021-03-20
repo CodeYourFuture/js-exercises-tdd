e8e02bd459ad69abcd0826ab72356dd1227aea8e
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-03-20 23:55:11 +0000

    Complete exercises for Find Needle

--- I.pass-tests/04-find-needle/find-needle.js
+++ I.pass-tests/04-find-needle/find-needle.js
@@ -1,3 +1,14 @@
-function findNeedle(words) {}
+function findNeedle(words, targetWord) {
+  const foundWords = words.reduce(function(acc, curr, index) {
+    if (curr === targetWord) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+
+  if (foundWords.length === 0) return "Target word not found";
+  if (foundWords.length === 1) return foundWords[0];
+  return foundWords;
+}
 
 module.exports = findNeedle;
