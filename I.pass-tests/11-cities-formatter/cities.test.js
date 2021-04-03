824954c78b46981d532e0b2694cd162769cb7f79
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-04-03 11:44:41 +0100

    Complete Cities Formatter exercise

--- I.pass-tests/11-cities-formatter/cities.test.js
+++ I.pass-tests/11-cities-formatter/cities.test.js
@@ -1,23 +1,42 @@
-var formatCities = require("./cities");
+const formatCities = require("./cities")
 
-test("Cities", function() {
-  var capitals = [
-    { city: "Paris", country: "France" },
-    { city: "Madrid", country: "Spain" },
-    { city: "Rome", country: "Italy" }
-  ];
-
+describe("Format cities", function() {
   function transform({ city, country }) {
-    return `${city} is the capital of ${country}`;
+    return `${city} is the capital of ${country}`
   }
 
-  var expected = [
-    "Paris is the capital of France",
-    "Madrid is the capital of Spain",
-    "Rome is the capital of Italy"
-  ];
+  test("It returns the capital city with the associated country", function() {
+    const capitals = [
+      { city: "Paris", country: "France" },
+      { city: "Madrid", country: "Spain" },
+      { city: "Rome", country: "Italy" }
+    ]
 
-  var result = formatCities(capitals, transform);
+    const expected = [
+      "Paris is the capital of France",
+      "Madrid is the capital of Spain",
+      "Rome is the capital of Italy"
+    ]
 
-  expect(result).toEqual(expected);
-});
+    const result = formatCities(capitals, transform)
+
+    expect(result).toEqual(expected)
+  })
+
+  test("It does not return capital city or country if either is not a string", function() {
+    const capitals = [
+      { city: 'Ankara', country: 'Turkey' },
+      { city: true, country: 'Lorem' },
+      { city: 'Berlin', country: 'Germany' },
+    ]
+
+    const expected = [
+      'Ankara is the capital of Turkey',
+      'Berlin is the capital of Germany',
+    ]
+
+    const result = formatCities(capitals, transform)
+
+    expect(result).toEqual(expected)
+  })
+})
