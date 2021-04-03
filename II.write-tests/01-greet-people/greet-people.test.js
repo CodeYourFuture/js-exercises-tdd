9d8b90d5db1e84edef8e169da88670947c9c463d
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-04-03 22:14:38 +0100

    Complete writing tests for greetPeople function

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,19 @@
-test("print list of names prefixed with Hello", function() {
-  // Arrange
-  // Act
-  // Assert
-});
+const greetPeople = require('./greet-people')
+
+describe("The greetPeople function", function() {
+  test("print list of names prefixed with Hello", function() {
+    // Arrange
+    // Act
+    // Assert
+
+    const mentors = ['Irina', 'Ashleigh', 'Etza']
+    const result = greetPeople(mentors)
+    const output = [
+      'Hello Irina',
+      'Hello Ashleigh',
+      'Hello Etza'
+    ]
+
+    expect(result).toEqual(output)
+  })
+})
