683f508ccea869019312b389cbdf1371b47b1fd6
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-04-04 10:14:42 +0100

    Complete writing tests for Numbers Greater Than 10 exercise

--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -1,6 +1,27 @@
-var largerThanTen = require("./numbersGreaterThan10");
+const largerThanTen = require("./numbersGreaterThan10");
 
-test("Get numbers greater than 10", function() {});
+describe("Larger than ten function", function() {
+  test("It ignores numbers less than 11", function() {
+    const numbers = [1, 4, -3, 0, 10]
+    const output = []
+
+    expect(largerThanTen(numbers)).toEqual(output)
+  })
+
+  test("It ignores data types that are not numbers", function() {
+    const arr = ['Hello', true, {}, '11', 100]
+    const output = [100]
+
+    expect(largerThanTen(arr)).toEqual(output)
+  })
+
+  test("It returns numbers greater than 10", function() {
+    const numbers = [7, 15, 50, 100, 1500, 9]
+    const output = [15, 50, 100, 1500]
+
+    expect(largerThanTen(numbers)).toEqual(output)
+  })
+})
 
 // input: [4, 10, 32, 9, 21];
 // expected output: [32, 21];
