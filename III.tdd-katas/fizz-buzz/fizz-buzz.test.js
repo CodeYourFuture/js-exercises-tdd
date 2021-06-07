ca39e183b253244f83f64ebc4e8e9939e48d4f85
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-06-07 13:53:03 +0100

    Complete FizzBuzz TDD exercise

--- III.tdd-katas/fizz-buzz/fizz-buzz.test.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.test.js
@@ -1,11 +1,34 @@
-var fizzbuzz = require("./fizz-buzz");
+const fizzBuzz = require('./fizz-buzz')
 
-test("prints 1", function() {
-  var result = fizzbuzz(1);
-  expect(result).toEqual("1");
-});
+describe('FizzBuzz function', function () {
+  jest.spyOn(console, 'log')
 
-test("prints 1,2", function() {
-  var result = fizzbuzz(2);
-  expect(result).toEqual("1, 2");
-});
+  beforeEach(() => {
+    console.log = jest.fn()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('prints number if not divisble by 3, 5 or both', function () {
+    fizzBuzz(1)
+    expect(console.log).toBeCalledTimes(1)
+    expect(console.log.mock.calls[0][0]).toBe(1)
+  })
+
+  test('prints Fizz if number is divisible by 3', function () {
+    fizzBuzz(6)
+    expect(console.log.mock.calls.flat()).toContain('Fizz')
+  })
+
+  test('prints Buzz if number is divisible by 5', function () {
+    fizzBuzz(15)
+    expect(console.log.mock.calls.flat()).toContain('Buzz')
+  })
+
+  test('prints FizzBuzz if number is divisible by 3 and 5', function () {
+    fizzBuzz(30)
+    expect(console.log.mock.calls.flat()).toContain('FizzBuzz')
+  })
+})
