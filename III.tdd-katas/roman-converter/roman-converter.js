d79d4747f093b986f309dddc5eb495df76b4a111
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-06-10 17:47:53 +0100

    Complete Roman Converter exercise

---
+++ III.tdd-katas/roman-converter/roman-converter.js
@@ -0,0 +1,54 @@
+function convertToOldRoman(number) {
+  let str = ''
+
+  const arabicToOldRomanMapper = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
+  }
+
+  for (prop in arabicToOldRomanMapper) {
+    while (number >= arabicToOldRomanMapper[prop]) {
+      str += prop
+      number -= arabicToOldRomanMapper[prop]
+    }
+  }
+  return str
+}
+
+function convertToNewRoman(number) {
+  let str = ''
+
+  const arabicToNewRomanMapper = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+
+  for (prop in arabicToNewRomanMapper) {
+    while (number >= arabicToNewRomanMapper[prop]) {
+      str += prop
+      number -= arabicToNewRomanMapper[prop]
+    }
+  }
+  return str
+}
+
+module.exports = {
+  convertToOldRoman,
+  convertToNewRoman
+}
