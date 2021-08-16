02dc84cd855d0a656a9f6725f2ee233573087dc7
yunusfirat <yunusfirat44@gmail.com>
2021-08-16 17:50:52 +0100

    intendation

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,12 +1,10 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
-function average(numbers){
+function average(numbers) {
     let arr = numbers.filter((num) => typeof num !== "string")
-   return arr.reduce((a,b) => a + b)/arr.length
-  }
-  
-  
-  module.exports=average;
+    return arr.reduce((a, b) => a + b) / arr.length
+}
 
-  console.log(average([4, "-", 8, 11, "hello", "57", 0, 2]))
+
+module.exports = average;
